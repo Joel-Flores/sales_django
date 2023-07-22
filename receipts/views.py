59c048019c2fdfa17aa1ea0760c682326ff57e07
Joel Flores <joel.flores3000@protonmail.com>
2023-07-22 13:40:34 -0400

    agregado y terminado de elimiar iten=ms del carrito limpiar el carrito completamente ver una tabla del carrito con el fomulario de que cliente lo pagara

--- receipts/views.py
+++ receipts/views.py
@@ -1,10 +1,36 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
 from django.views import View
+
+from src.helper import HelperApp
+from .forms import FormClient
 
 # Create your views here.
 class PayOrder(View):
     def get(self, request, *args, **kwarg):
+        form = FormClient()
+        orders = HelperApp.get_order(request)
+        orders = False if len(orders) == 0 else orders
+        total = HelperApp.get_total(request) if orders else None
         context = {
-            
+            'form' : form,
+            'orders' : orders,
+            'total' : total
         }
-        return render(request, 'home.html', context, )
+        return render(request, 'receipts/view_orders.html', context, )
+    
+    def post(self, request, *args, **kwarg):
+        form = FormClient(request.POST)
+        if not form.is_valid():
+            orders = HelperApp.get_order(request)
+            orders = False if len(orders) == 0 else orders
+            total = HelperApp.get_total(request) if orders else None
+            context = {
+                'form' : form,
+                'orders' : orders,
+                'total' : total
+            }
+            return render(request, 'receipts/view_orders.html', context, )
+        
+        
+        return redirect('home')
